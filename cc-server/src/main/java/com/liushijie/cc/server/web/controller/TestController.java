310936d207653c1378a6ce56cd036168ad774d27
liushijie <ofdata.z@gmail.com>
2016-10-06 17:31:19 +0800

    pom文件调整

--- cc-server/src/main/java/com/liushijie/cc/server/web/controller/TestController.java
+++ cc-server/src/main/java/com/liushijie/cc/server/web/controller/TestController.java
@@ -2,6 +2,7 @@ package com.liushijie.cc.server.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by liushijie on 16-9-5.
@@ -9,4 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class TestController {
+    @RequestMapping("test")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+
+    @RequestMapping("")
+    @ResponseBody
+    public String index() {
+        return "index";
+    }
 }
