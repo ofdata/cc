060ec82c6ca8ca83278235fbffbb8ac56926df9f
liushijie3 <liushijie@jd.com>
2017-06-02 18:30:13 +0800

    大框开发

--- cc-client/src/main/java/com/liushijie/cc/client/ConfigClient.java
+++ cc-client/src/main/java/com/liushijie/cc/client/ConfigClient.java
@@ -1,19 +1,18 @@
 package com.liushijie.cc.client;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * Created by liushijie on 5/30/17.
  */
 public class ConfigClient {
 
+    private final ConcurrentHashMap<String, String> localCache = new ConcurrentHashMap<String, String>();
 
 
     static {
         // 开启同步任务
-
+        init();
     }
 
     private static void init() {
@@ -28,10 +27,47 @@ public class ConfigClient {
     }
 
 
-    public void get(String group, String dataId) {
+    /**
+     * 获取全量的缓存数据
+     * @param key
+     * @return
+     */
+    public String getConfigInfo(String key) {
+        String value = null;
+        if( localCache.containsKey(key)) {
+            value = localCache.get(key);
+            if (value == null) {
+                value = loadFromRemote(key);
+                if (value == null) {
+                    value = loadFromSnap(key);
+                }
+            }
+        }
 
-
+        return value;
     }
+
+
+    /**
+     * 从远程网络获取信息
+     * @param key
+     * @return
+     */
+    private String loadFromRemote(String key) {
+        // TODO
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    /**
+     * 从快照获取配置信息
+     * @param key
+     * @return
+     */
+    private String loadFromSnap(String key) {
+        // TODO
+        throw new UnsupportedOperationException("TODO");
+    }
+
 
 
 
